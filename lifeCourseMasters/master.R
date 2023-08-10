32d4533be49e773f0de98c5645b5ad605396616e
TDJamieson <drtjamieson@gmail.com>
2023-08-10 10:18:31 +0100

    Folder name change

--- lifeCourseMasters/master.R
+++ lifeCourseMasters/master.R
@@ -48,7 +48,7 @@
       
       RProjectDirectory <- paste0(rootDirectory,"MSMicrosimulationModel_withADATesting/")
       
-      source(paste0(RProjectDirectory, 'masters/', 'masterArgs_local.R'))
+      source(paste0(RProjectDirectory, 'lifeCourseMasters/', 'masterArgs_local.R'))
       
     
       } else {
@@ -58,7 +58,7 @@
       
       RProjectDirectory <- paste0(rootDirectory,"MSMicrosimulationModel_withADATesting/")
       
-      source(paste0(RProjectDirectory, 'masters/', 'masterArgs_cluster.R'))
+      source(paste0(RProjectDirectory, 'lifeCourseMasters/', 'masterArgs_cluster.R'))
       
       
     }
